cb558126d9464c1f96ac8c8a764137299dd04bcf
RanShoshan <ranshoransho@gmail.com>
2018-06-02 15:49:53 +0300

    finished step 1 on final project

--- app/src/main/java/com/ran/finalproject/MainActivity.java
+++ app/src/main/java/com/ran/finalproject/MainActivity.java
@@ -1,13 +1,52 @@
 package com.ran.finalproject;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    MediaPlayer fullsong;
+    MediaPlayer beep;
+    AudioManager am;
+    int currentvol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        beep = MediaPlayer.create(MainActivity.this , R.raw.firebow);
+        fullsong= MediaPlayer.create(MainActivity.this,R.raw.underpressure);
+        fullsong.setLooping(true);
+        fullsong.start();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        currentvol = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+        if(currentvol == 0)
+            fullsong.pause();
+        else fullsong.start();
+    }
+
+
+    public void StartGame(View view) {
+        beep.start();
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void Setting(View view) {
+        beep.start();
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/ran/finalproject/SettingsActivity.java
@@ -0,0 +1,54 @@
+package com.ran.finalproject;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class SettingsActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    SeekBar sb;
+    AudioManager am;
+    TextView mv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        mv = findViewById(R.id.muteView);
+        sb = findViewById(R.id.seekBar);
+        am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        int maxvol = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentvol = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+        sb.setMax(maxvol);
+        sb.setProgress(currentvol);
+        sb.setOnSeekBarChangeListener(this);
+        mv.setText(String.valueOf(currentvol));
+    }
+
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if(progress == 0){
+            mv.setText("MUTED");
+        }
+        else
+            mv.setText(String.valueOf(progress));
+        am.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
